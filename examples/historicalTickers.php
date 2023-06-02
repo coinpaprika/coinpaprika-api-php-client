5255278a13884d6e56db91e3e6d51b2e1a32d123
Amir Hassani <a.hassani@phinix.com>
2023-06-02 11:50:44 +0330

    fix historical ticker comments

--- examples/historicalTickers.php
+++ examples/historicalTickers.php
@@ -5,7 +5,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $client = new \Coinpaprika\Client();
 
 // this will print historical tricks for given coin
-// in free version it only supports start from yesterday
+// valid interval values:
+// Free: 24h
+// Starter: 24h
+// Pro: 24H
+// Business: 1h, 6h, 12h, 24h
+// Enterprise: 15m, 30m, 1h, 6h, 12h, 24h
 foreach ($client->getHistoricalTickerByCoinId('fake-coin', '2023-05-23', '1d') as $ticker) {
     echo sprintf(
         "Timestamp: %s, Price: %s, Volume24H: %s, MarketCap: %s\n",
